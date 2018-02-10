cbf42a883899a1c4d45d6a48eb5077c094d70228
Shane Burkhart <shaneburkhart@gmail.com>
2018-02-09 22:51:22 -0600

    Put back the rails default prod logs.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,15 +73,19 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # So we can see the logs from the docker container's logs.
-  # STDOUT needs to be an ActiveSupport::Logger for Rails.
-  logger = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
+  # Use a different logger for distributed setups.
+  # require 'syslog/logger'
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
 end
