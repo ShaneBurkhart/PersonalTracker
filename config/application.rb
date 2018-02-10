aee9e27f9e56fb9f75e7708fcdddf6194636da31
Shane Burkhart <shaneburkhart@gmail.com>
2018-02-09 23:07:43 -0600

    Don't buffer logs.

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module PersonalTracker
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Don't buffer logs. We can't see them otherwise.
+    $stdout.sync = true
   end
 end
